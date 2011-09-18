46eac143629d72a0af12b6c2c7b67e94bc7f1657
Kaspars Foigts <laacz@laacz.lv>
2011-09-18 22:44:54 +0300

    Add config.dist.php.

---
+++ config.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+// Google (GMail) user and password
+$email = 'user@gmail.com';
+$password = '********';
+
+// Apps user and password
+$email2 = 'user@domain.com';
+$password2 = '*********';
